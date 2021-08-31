9f2698368b1bbca21946e60339ebabc5e7e148e5
lucasfavero <lucas.favero1994@gmail.com>
2021-08-31 11:17:37 -0300

    Update blackjack program.

--- 11 - Beginner - Blackjack 21/blackjack.py
+++ 11 - Beginner - Blackjack 21/blackjack.py
@@ -28,14 +28,19 @@ while keep_playing:
     if 21 >= sum(player_cards) > sum(computer_cards):
         print(f"Computer hand: {computer_cards} -> {sum(computer_cards)}")
         print(f"You won! {player_cards} -> {sum(player_cards)} ")
-    elif sum(player_cards) == sum(computer_cards):
+    elif 21 >= sum(computer_cards) > sum(player_cards):
+        print(f"Player hand: {player_cards} -> {sum(player_cards)} ")
+        print(f"Computer won! {computer_cards} -> {sum(computer_cards)} ")
+    elif sum(computer_cards) > 21:
+        print(f"Computer hand: {computer_cards} -> {sum(computer_cards)}")
+        print(f"You won! {player_cards} -> {sum(player_cards)} ")
+    elif sum(player_cards) > 21:
+        print(f"Player hand: {player_cards} -> {sum(player_cards)} ")
+        print(f"Computer won! {computer_cards} -> {sum(computer_cards)} ")
+    else:
         print(f"Computer hand: {computer_cards} -> {sum(computer_cards)}")
         print(f"Player hand: {player_cards} -> {sum(player_cards)} ")
         print(f"Draw!")
-    else:
-        print(f"Player hand: {player_cards} -> {sum(player_cards)} ")
-        print(f"Computer won! {computer_cards} -> {sum(computer_cards)} ")
-
     play_again = input(f"Do you want play again? Type 'y' or 'n': ")
 
     if play_again == 'n':
