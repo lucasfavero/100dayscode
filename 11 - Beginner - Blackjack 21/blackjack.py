29d35db5b2f70db395103b8bb29567fca48a41d6
lucasfavero <lucas.favero1994@gmail.com>
2021-08-31 11:03:52 -0300

    Update blackjack program.

--- 11 - Beginner - Blackjack 21/blackjack.py
+++ 11 - Beginner - Blackjack 21/blackjack.py
@@ -8,7 +8,7 @@ while keep_playing:
     computer_cards = random.choices(cards, k=2)
 
     print(f"Player hand: {player_cards}")
-    print(f"Computer hand: [{computer_cards[0]}]")
+    print(f"Computer hand: [{computer_cards[0]}] + X")
 
     another_card = input("Do you want another card? Type 'y' or 'n': ")
 
@@ -17,17 +17,21 @@ while keep_playing:
         if sum(player_cards) > 21:
             break
 
-        if sum(computer_cards) < 16:
+        if sum(computer_cards) < 17:
             computer_cards += random.choices(cards, k=1)
 
         print(f"Player hand: {player_cards} -> {sum(player_cards)}")
-        print(f"Computer hand: {computer_cards} -> {sum(computer_cards)}")
+        print(f"Computer hand: {computer_cards[:-1]} + X -> {sum(computer_cards[:-1])}")
 
         another_card = input("Do you want another card? Type 'y' or 'n': ")
 
     if 21 >= sum(player_cards) > sum(computer_cards):
         print(f"Computer hand: {computer_cards} -> {sum(computer_cards)}")
         print(f"You won! {player_cards} -> {sum(player_cards)} ")
+    elif sum(player_cards) == sum(computer_cards):
+        print(f"Computer hand: {computer_cards} -> {sum(computer_cards)}")
+        print(f"Player hand: {player_cards} -> {sum(player_cards)} ")
+        print(f"Draw!")
     else:
         print(f"Player hand: {player_cards} -> {sum(player_cards)} ")
         print(f"Computer won! {computer_cards} -> {sum(computer_cards)} ")
