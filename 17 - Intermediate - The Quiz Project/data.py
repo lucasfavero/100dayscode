281bbcf9537f3ec0c190030821ea34fb661b545f
lucasfavero <lucas.favero1994@gmail.com>
2021-09-08 21:50:20 -0300

    add quiz game OOP

---
+++ 17 - Intermediate - The Quiz Project/data.py
@@ -0,0 +1,33 @@
+# https://opentdb.com/api_config.php
+
+question_data = [
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The HTML5 standard was published in 2014.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The common software-programming acronym &quot;I18N&quot; comes from the term "
+                 "&quot;Interlocalization&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The very first recorded computer &quot;bug&quot; was a moth found inside a Harvard Mark II computer.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book "
+                 "form. ",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The open source program Redis is a relational database server.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                                      "question": "The first dual-core CPU was the Intel Pentium D.",
+                                      "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "Linux was first created as an alternative to Windows XP.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+                                      "question": "Android versions are named in alphabetical order.",
+                                      "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, Android, "
+                 "and Chrome OS.",
+     "correct_answer": "False", "incorrect_answers": ["True"]}]
